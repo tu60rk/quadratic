32ec73d633700db2598c2e8989fc333ce25804a8
tu60rk <tu60rk@yandex.ru>
2019-02-25 21:49:08 +0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -2,6 +2,14 @@ import cmath
 import math
 import sys
 
+
+SQUARED = "\N{SUPERSCRIPT TWO}"
+ARROW = "\N{RIGHTWARDS ARROW}"
+if not sys.platform.startswith("linux"):
+    SQUARED = "^2"
+    ARROW = "->"
+
+
 def get_float(msg, allow_zero):
     x = None
     while x is None:
@@ -13,26 +21,38 @@ def get_float(msg, allow_zero):
         except ValueError as err:
             print(err)
     return x
-    print("ax\N{SUPERSCRIPT TWO} + bx + c = 0")
-    a = get_float("enter a: ", False)
-    b = get_float("enter b: ", True)
-    c = get_float("enter c: ", True)
 
-    x1 = None
-    x2 = None
-    discriminant = (b ** 2)-(4 * a * c)
-    if discriminant == 0:
-        x1 = -(b / (2 * a))
+
+print("ax" + SQUARED + " + bx + c = 0")
+a = get_float("enter a: ", False)
+b = get_float("enter b: ", True)
+c = get_float("enter c: ", True)
+
+x1 = None
+x2 = None
+discriminant = (b ** 2) - (4 * a * c)
+if discriminant == 0:
+    x1 = -(b / (2 * a))
+else:
+    if discriminant > 0:
+        root = math.sqrt(discriminant)
+    else: # discriminant < 0
+        root = cmath.sqrt(discriminant)
+    x1 = (-b + root) / (2 * a)
+    x2 = (-b - root) / (2 * a)
+
+equation = "{0}x{1} ".format(a, SQUARED)
+if b != 0:
+    if b < 0:
+        equation += "- {0}x ".format(abs(b))
     else:
-        if discriminant > 0:
-            root = math.sqrt(discriminant)
-        else:
-            root = cmath.sqrt(discriminant)
-        x1 = (-b + root) / (2 * a)
-        x2 = (-b - root) / (2 * a)
-
-    equation = ("{0}x\N{SUPERSCRIPT TWO} + {1}x + {2} = 0"
-                " \N{RIGHTWARDS ARROW} x = {3}").format(a, b, c, x1)
-    if x2 is not None:
-        equation += " or x = {0}".format(x2)
-    print(equation)
+        equation += "+ {0}x ".format(b)
+if c != 0:
+    if c < 0:
+        equation += "- {0} ".format(abs(c))
+    else:
+        equation += "+ {0} ".format(c)
+equation += "= 0 {1} x = {0}".format(x1, ARROW)
+if x2 is not None:
+    equation += " or x = {0}".format(x2)
+print(equation)
